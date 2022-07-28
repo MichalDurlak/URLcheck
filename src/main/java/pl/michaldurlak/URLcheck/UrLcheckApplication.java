b35a39c9e98dfd75b561cd1027a735ff667bd67d
Michał Durlak <durlak.m@wp.pl>
2022-07-28 14:06:01 +0200

    Initial commit

---
+++ src/main/java/pl/michaldurlak/URLcheck/UrLcheckApplication.java
@@ -0,0 +1,13 @@
+package pl.michaldurlak.URLcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrLcheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrLcheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/michaldurlak/URLcheck/UrLcheckApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.michaldurlak.URLcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrLcheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
